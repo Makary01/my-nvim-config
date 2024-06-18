be630b11c94e5adb4af8a0e7d6a33720c05aa5ec
Makary01 <makary.bortnowski@gmail.com>
2024-06-18 09:38:45 +0200

    fix recent files plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,11 +5,10 @@ vim.keymap.set('n', '<leader>pl', function() builtin.live_grep(); end, {desc = "
 
 vim.keymap.set('n', '<leader>pw', function()
     local word = vim.fn.expand('<cword>')
-    builtin.grep_string({ search = word})
+    builtin.grep_string({ search = " " .. word})
 end, {desc = 'Grep current word'})
 
 local telescope = require('telescope')
-telescope.load_extension("recent_files")
 telescope.setup{
     defaults = {
         file_ignore_patterns = {
@@ -23,9 +22,19 @@ telescope.setup{
     extensions = {
         recent_files = {
             only_cwd = true,
+            transform_file_path = function (file_path)
+                -- remove directories from recent files
+                if file_path:match("%.[^\\/]+$") then
+                    return file_path
+                else
+                    return ""
+                end
+            end
         }
     }
 }
+-- this has to be after telescope.setup
+telescope.load_extension("recent_files")
 
 vim.api.nvim_set_keymap("n", "<C-p>",
   [[<cmd>lua require('telescope').extensions.recent_files.pick()<CR>]],
